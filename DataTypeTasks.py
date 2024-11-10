e01e67a25e1914c4119f6b2f8e89bf1619a27af2
Дмитрий <papanapulas@gmail.com>
2024-11-10 19:58:32 +0300

    10/11/2024 DataTypeTasks

---
+++ DataTypeTasks.py
@@ -0,0 +1,15 @@
+# 1st program
+print (9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+a = (2 * 2 + 2)
+b = (2 * (2 + 2))
+print(a)
+print(b)
+print(a == b)
+
+# 4th program
+print(int (float ('123.456') * 10) % 10)
